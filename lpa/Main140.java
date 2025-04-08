73564eaba130b22b8e9d2ba558e7f23637d624ea
Aadi Shrivastava <aadishrivastava13@gmail.com>
2025-04-08 16:43:37 +0530

    new file added

---
+++ lpa/Main140.java
@@ -0,0 +1,9 @@
+package dev.lpa;
+import java.util.*;
+public class Main140 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n%2==0?"Mahmoud":"Ehab");
+    }
+}
