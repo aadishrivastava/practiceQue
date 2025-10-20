e971bbdb9281ab66ab23148b099d48a21220cc08
Aadi Shrivastava <aadishrivastava13@gmail.com>
2025-10-20 16:08:36 +0530

    new file added

---
+++ lpa/Main242.java
@@ -0,0 +1,31 @@
+package dev.lpa;
+import java.util.*;
+
+public class Main242 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            Map<Integer, Integer> freq = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+                freq.put(a[i], freq.getOrDefault(a[i], 0) + 1);
+            }
+
+            int maxResult = 0;
+            for (int x : freq.values()) {
+                int occur = 0;
+                for (int y : freq.values()) {
+                    if (y >= x) occur++;
+                }
+                int result = occur * x;
+                maxResult = Math.max(maxResult, result);
+            }
+
+            System.out.println(maxResult);
+        }
+    }
+}
