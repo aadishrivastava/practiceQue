017a3e77e88ccbb372f8de5e5980e87a259f4c4b
Aadi Shrivastava <aadishrivastava13@gmail.com>
2025-11-20 11:07:48 +0530

    new file added

---
+++ lpa/Main307.java
@@ -0,0 +1,21 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main307 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        if(arr[2]-arr[0]>=10){
+            System.out.println("check again");
+        }else{
+            System.out.println("final "+arr[1]);
+        }
+
+    }
+}
